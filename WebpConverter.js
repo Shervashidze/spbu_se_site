1862fbebc9fe9e7239d6ef692e7f2e25bb31967f
George <33333462+Shervashidze@users.noreply.github.com>
2021-04-05 13:20:50 +0300

    Update WebpConverter.js

--- WebpConverter.js
+++ WebpConverter.js
@@ -1,18 +1,11 @@
-var imagemin = require("imagemin"),    // The imagemin module.
-  webp = require("imagemin-webp"),   // imagemin's WebP plugin.
-  outputFolder = "./docs/assets/img",            // Output folder
-  PNGImages = "./docs/assets/img/*.png",         // PNG images
-  JPEGImages = "./docs/assets/img/*.jpg";        // JPEG images
-  
-imagemin([PNGImages], outputFolder, {
-  plugins: [webp({
-      lossless: true // Losslessly encode images
-  })]
-});
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
 
-imagemin(['docs/assets/img/*.jpg'], {
-    destination: __dirname + '/docs/assets/img',
-    plugins: [
-        webp({quality: 65})
-    ]
-});
+(async () => {
+  const files = await imagemin(['./docs/assets/images/*.{jpg}'], {
+    destination: 'docs/assets/images',
+    plugins: [imageminWebp({ quality: 70 })],
+  });
+
+  console.log(files);
+})();
