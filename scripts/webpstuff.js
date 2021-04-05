408145a9a07f59c8da7c2d2cb4c115403984fbe7
George <gshervashidze1999@yandex.ru>
2021-04-05 16:06:36 +0300

    3 scripts now

--- scripts/webpstuff.js
+++ scripts/webpstuff.js
@@ -3,7 +3,7 @@ const imageminWebp = require('imagemin-webp');
 
 (async () => {
   const files = await imagemin(['./docs/assets/images/stuff/*.{jpg,png}'], {
-    destination: '../docs/assets/images/stuff',
+    destination: 'docs/assets/images/stuff',
     plugins: [imageminWebp({ quality: 70 })],
   });
 
