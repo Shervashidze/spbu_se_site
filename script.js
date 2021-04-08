36231a3701f0e1d879d44d8eeb375ea98b3c91d0
George <33333462+Shervashidze@users.noreply.github.com>
2021-04-08 19:05:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var glob = require('glob');
 var CWebp = require('cwebp').CWebp;
 var DWebp = require('cwebp').DWebp;
+const path = require('path')
 
 
 glob('./docs/**/*.jpg', function(err, files) {
 
   files.forEach(element => {
-
+    element = path.resolve(element);
+    
     var encoder = new CWebp(element);
     encoder.quality(65);
 
